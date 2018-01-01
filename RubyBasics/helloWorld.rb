e2fa361f0f978b98258dd5e2f74d579380bcad4b
GavinThomas1192 <gthomas1192@gmail.com>
2018-01-01 13:29:08 -0800

    first method

---
+++ RubyBasics/helloWorld.rb
@@ -0,0 +1,13 @@
+puts "Hello World"
+
+
+# //This prints to console on new line each time
+puts(1, 2, 'a', 'b')
+# Dont need parens to works
+puts 1, 2, 'a', 'b'
+# This puts the program to sleep for specified seconds
+sleep(2)
+# This prints to console on same line EX> $ cd
+print('c', 'd')
+# More sleep
+sleep(1)
