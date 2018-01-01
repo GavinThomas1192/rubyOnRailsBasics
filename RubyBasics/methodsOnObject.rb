a9e7ecb91ae5d1377668ed5b4578a6a9c38ff38a
GavinThomas1192 <gthomas1192@gmail.com>
2018-01-01 14:35:57 -0800

    object class

---
+++ RubyBasics/methodsOnObject.rb
@@ -0,0 +1,31 @@
+puts "AA".length
+
+string = "AA"
+number = 5
+
+puts string.length
+puts number.even?
+
+
+length = string.length
+
+puts "This is your calculated length #{length} from #{string}"
+puts "Is your calculated length even? #{string.length.even?}"
+
+# puts 3.odd?
+# puts 4.odd?
+# puts 3.even?
+# puts 4.even?
+
+# this wont work because its a string
+# puts "AA".odd?
+# this wont work because an integer doesn't have a length
+# puts 3.length
+
+# list a lot of methods you can do with associated class!!!
+# p 3.methods.sort
+# p "ee".methods.sort
+
+# to find class
+# p "aa".class = String
+# p 3.class = Fixnum
