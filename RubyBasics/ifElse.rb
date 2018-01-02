e60a2e170e30e6364d2b544c03d532c1ec5d047e
GavinThomas1192 <gthomas1192@gmail.com>
2018-01-02 10:47:59 -0800

    if else yolo:

---
+++ RubyBasics/ifElse.rb
@@ -0,0 +1,33 @@
+# if true
+#     puts "true"
+#     puts "additional code here"
+# end
+# if false
+#     puts "false"
+#     puts "additional code here"
+# end
+
+
+# if 75 > 50
+#     puts "75 > 50"
+# end 
+# if 75 > 100
+#     puts "75 > 100"
+# end
+# if 50 == 50
+#     puts "50 ==50"
+# end
+
+
+# here unless fires if the condition is false
+# so 75 is greater than 50, so the code block DOESNT RUN
+number = 75
+unless number > 50
+    puts "your number is larger than 50"
+end
+unless number > 200
+    puts "your number is larger than 200"
+end
+unless number > 75
+    puts "your number is larger than 75"
+end
--- RubyBasics/widgets.rb
+++ RubyBasics/widgets.rb
@@ -7,16 +7,29 @@ def ask(question)
     gets.chomp
 end
 
+def price(quantity)
+    if quantity >= 100
+        price_per_unit = 8
+        puts "DISCOUNT!!!!"
+    end
+    if quantity < 100
+        price_per_unit = 10
+        puts "NO DISCOUNT!!!"
+    end
+    quantity * price_per_unit
+end
+
 
 puts 'Welcome to the widgets store!'
 anwser = ask('How many widgets are you ordering?')
+
 puts "You entered #{anwser} widgets!"
-puts anwser
-# only p returns the whole ruby object
-p anwser.class
+number = anwser.to_i
+total = price(number)
+# p anwser.class
 # above returns "5/n" without chomp, "5" with chomp
 
 
 # We need to convert the anwser to a number to do math because gets always returns a string.
     
-puts "Your total price is $#{anwser.to_i * 10} "
+puts "Your total price is $#{total} "
