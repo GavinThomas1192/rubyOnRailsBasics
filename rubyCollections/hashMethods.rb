aa21cbe56880f0d1d548b27b5c7eb20a417eec30
GavinThomas1192 <gthomas1192@gmail.com>
2018-01-03 15:28:12 -0800

    finished hashes

--- rubyCollections/hashMethods.rb
+++ rubyCollections/hashMethods.rb
@@ -5,3 +5,20 @@ hash = {"item" => "bread", quantity: 1, brand: "Daves Killer Bread"}
 print "hash.length: "
 puts hash.length
 
+# returns all key value pairs inverted! pure method
+print "hash.invert: "
+puts hash.invert
+
+# removes the first key value pair mutatiting original hash
+print "hash.shift: "
+puts hash.shift.inspect
+print "hash after shift "
+puts hash.inspect
+
+puts hash["item"] = "bread"
+
+# this merges whatever you want, creating a new hash!
+# if you merge another hash with same key it will overwrite the old value!
+puts "merged with {'calories' => 100}"
+newHash = hash.merge({"calories" => 100})
+puts newHash
