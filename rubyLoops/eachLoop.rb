0b6cc7d77c5782c24059c8366299a330761124c0
GavinThomas1192 <gthomas1192@gmail.com>
2018-01-04 13:48:54 -0800

    math each loops!

--- rubyLoops/eachLoop.rb
+++ rubyLoops/eachLoop.rb
@@ -1,8 +1,20 @@
 array = [0, 1, 2, 3, 4, 5]
-i = 0
+# i = 0
 
-while i < array.length
-    item = array[i]
-    puts "The current item is #{item}"
-    i += 1
+# while i < array.length
+#     item = array[i]
+#     puts "The current item is #{item}"
+#     i += 1
+# end
+
+# array.each do |item|
+#     puts "The current item is #{item}"
+# end
+
+# array.each {|item| puts "The current item is #{item}"}
+
+array.each do |item|
+    
+    added =  item +2 
+    puts "#{item} + 2 = #{added}"
 end
