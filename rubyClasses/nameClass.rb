74803eae718e2ba5bbcd30abe786bb0f8b643faf
GavinThomas1192 <gthomas1192@gmail.com>
2018-01-05 09:13:09 -0800

    complicated classes, i smell javascript

--- rubyClasses/nameClass.rb
+++ rubyClasses/nameClass.rb
@@ -1,4 +1,39 @@
+# class Name
+    
+#     # here we run initialize, then assign an instance variable to title as @title so all the methods in the class can use title
+#     def initialize(title, first_name, middle_name, last_name)
+#         @title = title
+#         @first_name = first_name
+#         @middle_name = middle_name
+#         @last_name = last_name
+#     end
+
+#     def title
+#         @title
+#     end
+
+#     def first_name
+#         @first_name
+#     end
+
+#     def middle_name
+#         @middle_name
+#     end
+
+#     def last_name
+#         @last_name
+#     end
+# end
+
 class Name
+    # allows read access
+    attr_reader :title, :first_name, :middle_name, :last_name
+
+    # this allows write access on these variables
+    attr_writer :title, :first_name, :middle_name, :last_name
+
+    # this allows both read  AND WRITE
+    attr_accessor :title, :first_name, :middle_name, :last_name
 
     # here we run initialize, then assign an instance variable to title as @title so all the methods in the class can use title
     def initialize(title, first_name, middle_name, last_name)
@@ -8,32 +43,23 @@ class Name
         @last_name = last_name
     end
 
-    def title
-        @title
-    end
-
-    def first_name
-        @first_name
-    end
-
-    def middle_name
-        @middle_name
+    def full_name
+        @title + " " + @first_name + " " + @middle_name + " " + @last_name
     end
 
     def last_name
         @last_name
     end
-
-
 end
 
-# wont work if not correct number of arguments
+
 name = Name.new("Mr.", "Gavin", "Eugene", "Thomas" )
-# this shows where its located in memory only
-# puts name
 
+# puts name.title
+# name.title = "Dr."
+# puts name.title
 
-puts name.title + " " + name.first_name + " " + name.middle_name + " " + name.last_name + "."
+puts name.last_name
 
 # puts name.title
 # puts name.first_name
